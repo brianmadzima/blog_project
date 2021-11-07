66d5728368f0385a076fc4ebf95468e46cd9c93b
Brian Madzima <briyeeh@gmail.com>
2021-11-07 17:21:57 +0200

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+
+from django.views.generic.edit import CreateView, UpdateView
+
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
